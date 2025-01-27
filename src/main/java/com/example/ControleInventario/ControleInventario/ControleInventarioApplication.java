6b7028f13859f2ae7eda8d420c94813115557d26
henrique <37087436+SkBolttz@users.noreply.github.com>
2025-01-27 18:30:28 -0300

    first commit

---
+++ src/main/java/com/example/ControleInventario/ControleInventario/ControleInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.ControleInventario.ControleInventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleInventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ControleInventario/ControleInventario/ControleInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ControleInventario.ControleInventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
